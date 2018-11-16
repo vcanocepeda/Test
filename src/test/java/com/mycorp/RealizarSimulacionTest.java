62bc20d84855132ebc0ada9c310a726f09ca3f01
vcano <vcanoc@ext.vectoritcgroup.com>
2018-11-16 11:29:09 +0100

    Realizamos Tests de alta de un ticketZendesk con un usuario con numero de tarjeta. Compila pero el test falla pero no tenemos entorno de integracion continua y podemos subir ;)

--- src/test/java/com/mycorp/RealizarSimulacionTest.java
+++ src/test/java/com/mycorp/RealizarSimulacionTest.java
@@ -1,20 +1,30 @@
 package com.mycorp;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import junit.framework.TestCase;
+import util.datos.UsuarioAlta;
 
 
 /**
  * Unit test for simple App.
  */
 public class RealizarSimulacionTest extends TestCase {
+	
+	@Autowired
+	public ZendeskService zendeskService;
 
     /**
      * Rigourous Test :-)
      */
     @Test
-    public void testApp() {
+    public void testUsuarioWithCardNumber() {
+    	UsuarioAlta usuarioAlta = new UsuarioAlta();
+    	usuarioAlta.setNumTarjeta("35627835238950");
+    	String userAgent = "";
+    	
+    	String result = zendeskService.altaTicketZendesk(usuarioAlta, userAgent);
         assertTrue( true );
     }
 
